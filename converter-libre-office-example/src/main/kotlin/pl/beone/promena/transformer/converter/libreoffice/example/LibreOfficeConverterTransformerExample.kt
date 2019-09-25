8f91ab3205ca049016015ac564b2bb48c6e86eb1
Bartosz Skotarek <bzskotarek@gmail.com>
2019-09-25 11:08:10 +0200

    [improved] Adapted to new Promena plugin requirements

--- converter-libre-office-example/src/main/kotlin/pl/beone/promena/transformer/converter/libreoffice/example/LibreOfficeConverterTransformerExample.kt
+++ converter-libre-office-example/src/main/kotlin/pl/beone/promena/transformer/converter/libreoffice/example/LibreOfficeConverterTransformerExample.kt
@@ -3,10 +3,7 @@ package pl.beone.promena.transformer.converter.libreoffice.example
 import pl.beone.promena.transformer.contract.transformation.Transformation
 import pl.beone.promena.transformer.converter.libreoffice.applicationmodel.libreOfficeConverterTransformation
 
-fun transform(): Transformation {
-    // HTTP: localhost:8080
-    // Repeat: 1
-    // Concurrency: 1
+fun promena(): Transformation {
     // Data: example.txt
 
     return libreOfficeConverterTransformation()
