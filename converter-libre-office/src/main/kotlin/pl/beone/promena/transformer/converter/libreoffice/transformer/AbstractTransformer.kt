0ef3e0e68f8eaa79a8652c9c471f20af71826919
Bartosz Skotarek <bzskotarek@gmail.com>
2019-09-29 15:00:58 +0200

    [refactored] Function names

--- converter-libre-office/src/main/kotlin/pl/beone/promena/transformer/converter/libreoffice/transformer/AbstractTransformer.kt
+++ converter-libre-office/src/main/kotlin/pl/beone/promena/transformer/converter/libreoffice/transformer/AbstractTransformer.kt
@@ -54,7 +54,7 @@ internal abstract class AbstractTransformer(
 
         val (data, mediaType, metadata) = singleDataDescriptor
 
-        processData(data, mediaType).getInputStream().use { inputStream ->
+        process(data, mediaType).getInputStream().use { inputStream ->
             getOutputStream().use { outputStream ->
                 LocalConverter.make(officeManagerCoordinator.getManager())
                     .convert(inputStream)
@@ -68,7 +68,7 @@ internal abstract class AbstractTransformer(
         return singleTransformedDataDescriptor(createData(), metadata)
     }
 
-    private fun processData(data: Data, mediaType: MediaType): Data =
+    private fun process(data: Data, mediaType: MediaType): Data =
         dataProcessors
             .filter { it.isSupported(data, mediaType) }
             .fold(data) { currentData, dataProcessor -> dataProcessor.process(currentData, mediaType) }
