37ca5fdcd56c008bf9eac283ef58c99e12693cab
Bartosz Skotarek <bzskotarek@gmail.com>
2019-10-09 08:21:32 +0200

    [fixed] Mocking Support objects

--- implementation/src/test/kotlin/pl/beone/promena/transformer/converter/libreoffice/LibreOfficeConverterTransformerSupportTest.kt
+++ implementation/src/test/kotlin/pl/beone/promena/transformer/converter/libreoffice/LibreOfficeConverterTransformerSupportTest.kt
@@ -1,8 +1,6 @@
 package pl.beone.promena.transformer.converter.libreoffice
 
 import io.mockk.*
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import pl.beone.lib.junit.jupiter.external.DockerExtension
@@ -14,27 +12,18 @@ import pl.beone.promena.transformer.converter.libreoffice.applicationmodel.Libre
 @ExtendWith(DockerExtension::class)
 class LibreOfficeConverterTransformerSupportTest {
 
-    @BeforeEach
-    fun setUp() {
-        mockkObject(LibreOfficeConverterSupport)
-    }
-
     @Test
     fun isSupported() {
         val dataDescriptor = mockk<DataDescriptor>()
         val targetMediaType = mockk<MediaType>()
         val parameters = mockk<Parameters>()
 
+        mockkStatic(LibreOfficeConverterSupport::class)
         every { LibreOfficeConverterSupport.isSupported(dataDescriptor, targetMediaType, parameters) } just Runs
 
         LibreOfficeConverterTransformer(LibreOfficeConverterTransformerSettings(), LibreOfficeConverterTransformerDefaultParameters(), mockk())
             .isSupported(dataDescriptor, targetMediaType, parameters)
 
         verify(exactly = 1) { LibreOfficeConverterSupport.isSupported(dataDescriptor, targetMediaType, parameters) }
-    }
-
-    @AfterEach
-    fun tearDown() {
-        unmockkObject(LibreOfficeConverterSupport)
     }
 }
